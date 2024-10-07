909c3ad80152bfe81d8cca5139476ee62016ef17
ana.lopes <ana.lopes011@edu.pucrs.br>
2024-10-07 20:54:30 -0300

    Atualização MLP

--- backend/src/ai/algorithm/mlp.py
+++ backend/src/ai/algorithm/mlp.py
@@ -4,5 +4,5 @@ from sklearn.neural_network import MLPClassifier
 class MLP(BaseModelML):
     def __init__(self):
         super().__init__()
-        self.clf = MLPClassifier(solver='adam', hidden_layer_sizes=(3,), learning_rate_init=0.3, momentum=0.2, verbose=True)       
+        self.clf = MLPClassifier(solver='adam', hidden_layer_sizes=(2,12), learning_rate_init=0.008, momentum=0.5, verbose=True)       
         self._train()
