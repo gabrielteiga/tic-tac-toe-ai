756e9e98573e39376cc7c6c17860ece0d5f170fc
gabrielteiga <gabrielteiga99@hotmail.com>
2024-10-23 21:18:44 -0300

    feat: implementing the frontend t2 layout

--- frontend/src/components/TicTacToe/TicTacToe.jsx
+++ frontend/src/components/TicTacToe/TicTacToe.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useRef } from "react";
-import axios from 'axios';
+import React, { useState, useRef, useEffect } from "react";
 import "./TicTacToe.css";
 import circle_img from "../../assets/Red_circle.png";
 import cross_img from "../../assets/Red_X.png";
@@ -8,35 +7,129 @@ const TicTacToe = () => {
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
   const [data, setData] = useState(Array(9).fill("b"));
+  const [difficulty, setDifficulty] = useState("easy");
   const titleRef = useRef(null);
 
   const boxRefs = useRef([React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef()]);
-  const knnRef = useRef(null);
-  const treeRef = useRef(null);
-  const mlpRef = useRef(null);
+
+  const minimax = (newData, isMaximizing) => {
+    const winner = checkwin(newData);
+    if (winner === "x") return -1;
+    if (winner === "o") return 1;
+    if (newData.every(cell => cell !== "b")) return 0;
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < newData.length; i++) {
+        if (newData[i] === "b") {
+          newData[i] = "o";
+          let score = minimax(newData, false);
+          newData[i] = "b";
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < newData.length; i++) {
+        if (newData[i] === "b") {
+          newData[i] = "x";
+          let score = minimax(newData, true);
+          newData[i] = "b";
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+      return bestScore;
+    }
+  };
+
+  const bestMove = (newData) => {
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < newData.length; i++) {
+      if (newData[i] === "b") {
+        newData[i] = "o";
+        let score = minimax(newData, false);
+        newData[i] = "b";
+        if (score > bestScore) {
+          bestScore = score;
+          move = i;
+        }
+      }
+    }
+    debugBestMove();
+    return move;
+  };
+
+  const randomMove = (newData) => {
+    const availableMoves = newData.map((val, index) => val === "b" ? index : null).filter(val => val !== null);
+    debugRandomMove();
+    return availableMoves[Math.floor(Math.random() * availableMoves.length)];
+  };
 
   const toggle = async (e, num) => {
+    console.log("iniciando toggle")
     if (lock || data[num] !== "b") {
+      console.log('aqui entrou no if do lock toggle')
       return;
     }
-  
+    
+    console.log("count: ",count)
     const newData = [...data];
-    if (count % 2 === 0) {
+    // if (count % 2 === 0) {
       newData[num] = "x";
-      e.target.innerHTML = `<img src='${cross_img}' alt='X' class='symbol'>`;  
-    } else {
-      newData[num] = "o";
-      e.target.innerHTML = `<img src='${circle_img}' alt='O' class='symbol'>`; 
+      e.target.innerHTML = `<img src='${cross_img}' alt='X' class='symbol'>`;
+      setData(newData);
+      setCount(count + 2);
+      console.log("count: ",count)
+
+      const winner = checkwin(newData);
+      console.log(count)
+      console.log(winner)
+      if (winner) {
+        won(winner);
+      } else {
+        computerPlay(newData);
+      // }
     }
-    setData(newData);
-    setCount(count + 1);
-  
-    const args = newData.map(val => (val === "" ? "b" : val));
-  
-    checkwin(newData);
-    await checkKnn(args);
-    await checkTree(args);
-    await checkMlp(args);
+    console.log("finalizando toggle")
+  };
+
+  const computerPlay = (newData) => {
+    if (lock) return;
+
+    let computerMove;
+    const random = Math.random();
+    console.log("antes")
+    
+    if (difficulty === "easy") {
+      if (random < 0.25) {
+        computerMove = bestMove(newData);
+      } else {
+        computerMove = randomMove(newData);
+      }
+    } else if (difficulty === "medium") {
+      if (random < 0.5) {
+        computerMove = randomMove(newData);
+      } else {
+        computerMove = bestMove(newData);
+      }
+    } else if (difficulty === "hard") {
+      computerMove = bestMove(newData);
+    } else {
+      computerMove = randomMove(newData);
+    }
+
+    if (computerMove !== undefined) {
+      newData[computerMove] = "o";
+      boxRefs.current[computerMove].current.innerHTML = `<img src='${circle_img}' alt='O' class='symbol'>`;
+      setData(newData);
+      setCount(count + 1);
+      const winner = checkwin(newData);
+      if (winner) {
+        won(winner);
+      }
+    }
   };
 
   const checkwin = (newData) => {
@@ -45,13 +138,15 @@ const TicTacToe = () => {
       [0, 3, 6], [1, 4, 7], [2, 5, 8],
       [0, 4, 8], [2, 4, 6]
     ];
+
     for (const [a, b, c] of lines) {
-      if (newData[a] && newData[a] === newData[b] && newData[a] === newData[c] && newData[a] !== "b") {
-        won(newData[a]);
-        return;
+      if (newData[a] === newData[b] && newData[a] === newData[c] && newData[a] !== "b") {
+        return newData[a];
       }
     }
-  }
+
+    return null;
+  };
 
   const won = (winner) => {
     setLock(true);
@@ -62,82 +157,31 @@ const TicTacToe = () => {
     }
   }
 
-  const checkKnn = async (args) => {
-    try {
-      const response = await axios.post('http://localhost:8080/knn/predict', { data: args });
-      const winner = response.data;
-      updatePrediction(winner, knnRef, "KNN");
-    } catch (error) {
-      console.error(`Error in KNN prediction: ${error}`);
-    }
-  }
-
-  const checkTree = async (args) => {
-    try {
-      const response = await axios.post('http://localhost:8080/dtree/predict', { data: args });
-      const winner = response.data;
-      updatePrediction(winner, treeRef, "Tree");
-    } catch (error) {
-      console.error(`Error in Tree prediction: ${error}`);
-    }
-  }
-
-  const checkMlp = async (args) => {
-    try {
-      const response = await axios.post('http://localhost:8080/mlp/predict', { data: args });
-      const winner = response.data;
-      updatePrediction(winner, mlpRef, "MLP");
-    } catch (error) {
-      console.error(`Error in MLP prediction: ${error}`);
-    }
-  }
-
-  const updatePrediction = (winner, ref, type) => {
-    if (winner === "w") {
-      return;
-    }
-    
-    console.log(`Winner from ${type}:`, winner); 
-
-    const predictionText = getPredictionText(Number(winner));
-    ref.current.innerHTML = `${type}: ${predictionText}`;
-  }
-
-  const getPredictionText = (winner) => {
-    switch (winner) {
-      case 1:
-        return "";
-      case 2:
-        return "X ganhou";
-      case 3:
-        return "O ganhou";
-      case 4:
-        return "Em jogo";
-      case 5:
-        return "Empate";
-      default:
-        return "Não identificado";
-    }
-  }
-
   const reset = () => {
     setData(Array(9).fill("b"));
     setCount(0);
     setLock(false);
     titleRef.current.innerHTML = "";
-    knnRef.current.innerHTML = "";
-    treeRef.current.innerHTML = "";
-    mlpRef.current.innerHTML = "";
     boxRefs.current.forEach(ref => ref.current.innerHTML = "");
+  }
+
+  function debugBestMove() {
+    console.log("Best move");
+  }
+
+  function debugRandomMove() {
+    console.log("Random move");
   }
 
   return (
     <div className="container">
       <div className="inicial">
         <h1 className="title" ref={titleRef}></h1>
-        <div className="subtitle" ref={knnRef}></div>
-        <div className="subtitle" ref={treeRef}></div>
-        <div className="subtitle" ref={mlpRef}></div>
+        <select onChange={(e) => setDifficulty(e.target.value)} value={difficulty}>
+          <option value="easy">Fácil</option>
+          <option value="medium">Médio</option>
+          <option value="hard">Difícil</option>
+        </select>
       </div>
       <div className="game">
         <div className="board">
